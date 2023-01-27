d6fb24134c3dc84d21f6b7084c5b4ea0805a5f0c
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-01-27 17:01:11 +0800

    Position
    - IN PROGRESS

--- client/src/pages/main/maintenance/position/layouts/list/index.jsx
+++ client/src/pages/main/maintenance/position/layouts/list/index.jsx
@@ -1,54 +1,54 @@
 // Libraries
-import { useContext, useState } from "react";
+import { useContext, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisH, faFileArrowDown, faFileArrowUp, faMagnifyingGlass, faPlus } from "@fortawesome/free-solid-svg-icons";
-import { Box, FormLabel, Skeleton, Stack, TextField, Typography } from "@mui/material";
+import { Box, FormLabel, Stack, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 // Core
+import { GlobalCntx } from "core/context/Global"; // Context
 import { ListCntxt } from "core/context/List"; // Context
 import { ProfileCntx } from "core/context/Profile"; // Context
-import { exporttoexcel, importfromexcel, useGet, usePost } from "core/function/global"; // Function
+import { exporttoexcel, importfromexcel, usePost } from "core/function/global"; // Function
 import { excel, look, records, upload } from "core/api"; // API
 
 // Layouts
 import Dashboard from "./layouts/Dashboard";
 import Item from "./layouts/Item";
+import Loader from "./layouts/Loader";
 
 // Contants
-import { btnexport, btnicon, btnimport, btntxt, loader, search } from "./index.style"; // Styles
+import { btnexport, btnicon, btnimport, btntxt, search } from "./index.style"; // Styles
+import Sort from "./layouts/Sort";
 
 const Index = () => {
     let name = `KC-EXPORT-POSITION-${parseInt((new Date()).getMonth()) + 1}${(new Date()).getDate()}${(new Date()).getFullYear()}`;
     const { setList } = useContext(ListCntxt);
+    const { orderby, category, searchtxt, setSearchtxt, message, setMessage, errors, setErrors } = useContext(GlobalCntx);
     const { data } = useContext(ProfileCntx);
-    const [ message, setMessage ] = useState('');
-    const [ errors, setErrors ] = useState([]);
     const { mutate: find, isLoading: finding } = usePost({ fetch: look, onSuccess: (data) => setList(data) });
-    const { isFetching: fetching } = useGet({ key: ['pst_list'], fetch: records({ table: 'tbl_position', data: {} }), options: { refetchOnWindowFocuse: false }, onSuccess: (data) => setList(data) });
-    const { refetch: original} = 
-        useGet({ key: ['pst_original'], fetch: excel({ table: 'tbl_position', type: 'original' }), options: { enabled: false }, 
-            onSuccess: (data) => exporttoexcel(data, 'Position', `${name} (Admin's copy)`) 
-        });
-    const { refetch: formatted } = 
-        useGet({ key: ['pst_formatted'], fetch: excel({ table: 'tbl_position', type: 'formatted' }), options: { enabled: false }, 
-            onSuccess: (data) => exporttoexcel(data, 'Position', `${name}`) 
-        });
-    const { mutate: uploadfile, isLoading: uploading } = 
-        usePost({ fetch: upload, 
+    const { mutate: record, isLoading: fetching } = usePost({ fetch: records, options: { refetchOnWindowFocus: false }, onSuccess: (data) => setList(data) });
+
+    const { mutate: original } = usePost({ fetch: excel, options: { refetchOnWindowsFocus: false }, onSuccess: (data) =>  exporttoexcel(data, 'Position', `${name} (Admin's copy)`) });
+    const { mutate: formatted } = usePost({ fetch: excel, options: { refetchOnWindowsFocus: false }, onSuccess: (data) => exporttoexcel(data, 'Position', `${name}`) });
+
+    const { mutate: uploadfile, isLoading: uploading } =
+        usePost({ fetch: upload,
             onSuccess: (data) => {
                 setMessage('');
                 setErrors([]);
-                
+
                 setErrors(data.errors);
                 setMessage(`${data.success} out of ${data.total} row/s successfully imported! ${data.fail} row/s failed!`);
                 setList(data.list);
                 setTimeout(() => { setMessage(''); setErrors([]); }, 5000);
-            } 
+            }    
         });
 
+    useEffect(() => record({ table: 'tbl_position', data: { category: category, orderby: orderby, searchtxt: searchtxt }}), [ record, category, orderby, searchtxt ]);
+
     return (
-        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" sx= {{ width: '100%', height: '100%' }} spacing= { 3 }>
+        <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" sx= {{ width: '100%', overflow: 'hidden' }} spacing= { 1 }>
             <Stack direction= "column" justifyContent= "flex-start" alignItems= "stretch" spacing= { 1 }>
                 <Typography variant= "h6" sx= {{ fontFamily: 'Boldstrom', color: '#3C4048' }}>Position</Typography>
                 <Dashboard />
@@ -57,14 +57,23 @@ const Index = () => {
                         <Box sx= { search }>
                             <FontAwesomeIcon icon= { faMagnifyingGlass } size= "sm" style= {{ margin: '8px' }} />
                             <TextField variant= "standard" size= "small" fullWidth InputProps= {{ disableUnderline: true }} placeholder= "Search..." sx= {{ padding: '5px 0 0 0' }}
-                                onChange= { e => { find({ table: 'tbl_position', data: { condition: e.target.value !== '' ? (e.target.value).toUpperCase() : e.target.value } }); } } />
+                                onChange= { e => { 
+                                    setSearchtxt(e.target.value !== '' ? (e.target.value).toUpperCase() : e.target.value);
+                                    find({ table: 'tbl_position', data: { condition: e.target.value !== '' ? (e.target.value).toUpperCase() : e.target.value,
+                                                                                            category: category, orderby: orderby } }); } } />
                         </Box>
                     </form>
                     <Stack direction= "row" justifyContent= "flex-end" alignItems= "center" sx= {{ flexGrow: 1 }} spacing= { 1 }>
-                        <input type= "file" name= "upload-file" id= "upload-file" style= {{ width: '0.1px', height: '0.1px', opacity: 0, overflow: 'hidden', position: 'absolute', zIndex: -1 }} 
-                            onChange= { async e => { uploadfile({ table: 'tbl_position', data: { json: await importfromexcel(e), id: atob(localStorage.getItem('token')) } }); e.target.value = '' } } />
-                        <FormLabel htmlFor= "upload-file" sx= { btnimport }><FontAwesomeIcon icon= { !uploading ? faFileArrowUp : faEllipsisH } style= {{ color: '#FFFFFF' }} size= "lg" /></FormLabel>
-                        <Typography onClick= { () => { if(data.user_level === 'superadmin') { original(); } formatted(); }} sx= { btnexport }>
+                        { data.user_level === 'superadmin' ?
+                            <input type= "file" name= "upload-file" id= "upload-file" style= {{ width: '0.1px', height: '0.1px', opacity: 0, overflow: 'hidden', position: 'absolute', zIndex: -1 }}
+                                onChange= { async e => { uploadfile({ table: 'tb_position', data: { json: await importfromexcel(e), id: atob(localStorage.getItem('token')) } }); e.target.value = '' } } /> : ''}
+                        { data.user_level === 'superadmin' ? <FormLabel htmlFor= "upload-file" sx= { btnimport }>
+                            <FontAwesomeIcon icon= { !uploading ? faFileArrowUp : faEllipsisH } style= {{ color: '#FFFFFF'}} size= "lg" />
+                        </FormLabel> : '' }
+                        <Typography 
+                            onClick= { () => { 
+                                if(data.user_level === 'superadmin') { original({ table: 'tbl_position', type: 'original', condition: { orderby: orderby, category: category } }); } 
+                                formatted({ table: 'tbl_position', type: 'formatted', condition: { orderby: orderby, category: category, searchtxt: searchtxt } }); }} sx= { btnexport }>
                             <FontAwesomeIcon icon= { faFileArrowDown } style= {{ color: '#FFFFFF' }} size= "lg" />
                         </Typography>
                         <Typography component= { Link } to= "/maintenance/position/form/new" sx= { btnicon }>
@@ -79,21 +88,8 @@ const Index = () => {
                     </Stack>
                 </Stack>
             </Stack>
-            { !fetching && !finding ? <Item /> :  
-                <Stack direction= "row" justifyContent= "space-between" alignItems= "center" sx= { loader } spacing= { 2 }>
-                    <Stack direction= "column" justifyContent= "flex-start" alignItems= "flex-start" spacing= { 1 } sx= {{ flexGrow: 1 }}>
-                        <Skeleton variant= "rounded" sx= {{ width: '50%', height: '10px' }} />
-                        <Skeleton variant= "rounded" sx= {{ width: '25%', height: '10px' }} />
-                        <Skeleton variant= "rounded" sx= {{ width: '27%', height: '10px' }} />
-                    </Stack>
-                    <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" spacing= { 2 }>
-                        <Skeleton variant= "circular" sx= {{ width: '10px', height: '10px' }} />
-                        <Stack direction= "row" justifyContent= "flex-start" alignItems= "center" spacing= { 1 }>
-                            <Skeleton variant= "rounded" sx= {{ padding: '15px' }} />
-                            <Skeleton variant= "rounded" sx= {{ padding: '15px' }} />
-                        </Stack>
-                    </Stack>
-                </Stack> }
+            <Sort refetch= { record } />
+            <Box sx= {{ overflowY: 'scroll', '&::-webkit-scrollbar': { display: 'none' } }}>{ !fetching && !finding ? <Item /> : <Loader /> }</Box>
         </Stack>
     );
 }
