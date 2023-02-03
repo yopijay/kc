4bd02b36ca6a38df4c2b9a1a6c0fd1efb3068f31
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-02-03 17:00:21 +0800

    Update Navs.js

--- client/src/core/constants/Navs.js
+++ client/src/core/constants/Navs.js
@@ -10,35 +10,42 @@ const User = React.lazy(() => import('pages/main/maintenance/user'));
 export const Navs = () => ([
     {
         title: 'Main', nav: [
-            { path: '/', label: 'Dashboard', name: 'home', component: 'DASHBOARD', access: [ 'superadmin', 'admin', 'user' ] }
+            { path: '/', label: 'Dashboard', name: 'home', component: 'DASHBOARD' }
         ]
     },
     {
         title: 'Tools', nav: [
-            { path: '/tools/dsf', label: 'Daily Symptoms', name: 'dsf', component: "DAILY SYMPTOMS", access: [ 'superadmin', 'admin', 'user' ] },
-            { path: '/tools/free-meal', label: 'Free Meal', name: 'free-meal', component: "FREE MEAL", access: [ 'superadmin', 'admin', 'user' ] },
+            { path: '/tools/dsf', label: 'Daily Symptoms', name: 'dsf', component: "DAILY SYMPTOMS" },
+            { path: '/tools/free-meal', label: 'Free Meal', name: 'free-meal', component: "FREE MEAL" },
         ]
     },
     {
         title: 'Maintenance', nav: [
-            { path: '/maintenance/company', label: 'Company', name: 'company', component: <Company />, access: [ 'superadmin', 'admin' ] },
-            { path: '/maintenance/department', label: 'Department', name: 'department', component: <Department />, access: [ 'superadmin', 'admin' ] },
-            { path: '/maintenance/position', label: 'Position', name: 'position', component: <Position />, access: [ 'superadmin', 'admin' ] },
-            { path: '/maintenance/user', label: 'User', name: 'user', component: <User />, access: [ 'superadmin', 'admin' ] },
+            { path: '/maintenance/company', label: 'Company', name: 'company', component: <Company /> },
+            { path: '/maintenance/department', label: 'Department', name: 'department', component: <Department /> },
+            { path: '/maintenance/position', label: 'Position', name: 'position', component: <Position /> },
+            { path: '/maintenance/user', label: 'User', name: 'user', component: <User /> },
         ]
     },
+    // {
+    //     title: 'Warehouse', nav: [
+    //         { path: '/warehouse/products', label: 'Products', name: 'products', component: 'PRODUCTS' },
+    //     ]
+    // },
     {
-        title: 'Warehouse', nav: [
-            { path: '/warehouse/products', label: 'Products', name: 'products', component: 'PRODUCTS', access: [ 'superadmin', 'admin' ] },
-            { path: '/warehouse/office-supplies', label: 'Office Supplies', name: 'office-supplies', component: 'OFFICE SUPPLIES', access: [ 'superadmin', 'admin' ] },
+        title: 'Supplies', nav: [
+            { path: '/supplies/office-furniture', label: 'Office furniture', name: 'office-furniture', component: 'OFFICE FURNITURE' },
+            { path: '/supplies/technology', label: 'Technology', name: 'technology', component: 'TECHNOLOGY' },
+            { path: '/supplies/office-essentials', label: 'Office essentials', name: 'office-essentials', component: 'OFFICE ESSENTIALS' },
+            { path: '/supplies/breakroom', label: 'Breakroom', name: 'breakroom', component: 'Breakroom' },
         ]
     },
-    {
-        title: 'Assets', nav: [
-            { path: '/assets/category', label: 'Category', name: 'category', component: 'Category', access: [ 'superadmin', 'admin' ] },
-            { path: '/assets/brand', label: 'Brand', name: 'brand', component: 'brand', access: [ 'superadmin', 'admin' ] },
-            { path: '/assets/asset-registration', label: 'Asset Registration', name: 'asset-registration', component: 'ASSET REGISTRATION', access: [ 'superadmin', 'admin' ] },
-            { path: '/assets/asset-issuance', label: 'Asset Issuance', name: 'asset-issuance', component: 'ASSET ISSUANCE', access: [ 'superadmin', 'admin' ] },
-        ]
-    }
+    // {
+    //     title: 'Assets', nav: [
+    //         { path: '/assets/category', label: 'Category', name: 'category', component: 'Category' },
+    //         { path: '/assets/brand', label: 'Brand', name: 'brand', component: 'brand' },
+    //         { path: '/assets/asset-registration', label: 'Asset Registration', name: 'asset-registration', component: 'ASSET REGISTRATION' },
+    //         { path: '/assets/asset-issuance', label: 'Asset Issuance', name: 'asset-issuance', component: 'ASSET ISSUANCE' },
+    //     ]
+    // }
 ])
