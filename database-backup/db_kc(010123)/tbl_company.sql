ee741fb44109b05e64dedf2cea4d7e63c49c79db
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-03-01 15:54:12 +0800

    Live server

--- database-backup/db_kc(010123)/tbl_company.sql
+++ database-backup/db_kc(010123)/tbl_company.sql
@@ -17,6 +17,18 @@
 
 
 -- ----------------------------
+-- Table sequence for tbl_company
+-- ----------------------------
+CREATE SEQUENCE "public"."tbl_company_id_seq" 
+INCREMENT 1
+MINVALUE  1
+MAXVALUE 2147483647
+START 1
+CACHE 1;
+
+SELECT setval('"public"."tbl_company_id_seq"', 5, true);
+
+-- ----------------------------
 -- Table structure for tbl_company
 -- ----------------------------
 DROP TABLE IF EXISTS "public"."tbl_company";
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 
 // Variables
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 // Middleware
 app.use(cors());
