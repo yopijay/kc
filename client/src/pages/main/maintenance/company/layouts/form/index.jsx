93b901b57e4afeb8689c8ecaf88ab408b8b2e376
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-03-10 16:53:57 +0800

    Trial & Error

--- client/src/pages/main/maintenance/company/layouts/form/index.jsx
+++ client/src/pages/main/maintenance/company/layouts/form/index.jsx
@@ -47,12 +47,13 @@ const Index = () => {
     const { mutate: saving } = 
         usePost({ fetch: save, 
             onSuccess: (data) => { 
-                if(data.result === 'error') { 
-                    (data.error).forEach((err, index) => { 
-                        setError(err.name, { type: index === 0 ? 'focus' : '', message: err.message }, { shouldFocus: index === 0 }); 
-                    });
-                } 
-                else { successToast(data.message, 3000, navigate('/maintenance/company', { replace: true })); } 
+                console.log(data);
+                // if(data.result === 'error') { 
+                //     (data.error).forEach((err, index) => { 
+                //         setError(err.name, { type: index === 0 ? 'focus' : '', message: err.message }, { shouldFocus: index === 0 }); 
+                //     });
+                // } 
+                // else { successToast(data.message, 3000, navigate('/maintenance/company', { replace: true })); } 
             } 
         });
 
