cb1fd558a61b52b685798f685535637f355b5d52
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-03-10 14:27:33 +0800

    Update index.js

--- client/src/core/api/index.js
+++ client/src/core/api/index.js
@@ -2,7 +2,7 @@
 import { api } from "core/function/global";
 import Env from '../constants/Env.json';
 
-const env = 'lan'; // Environment
+const env = 'live'; // Environment
 
 export const authentication = async (data) => { return await api({ url: `${Env[env].url}/login`, method: 'post', data: data }).then(res => res.data); }
 export const logout = async (data) => { return await api({ url: `${Env[env].url}/logout`, method: 'post', data: data }).then(res => res.data); }
