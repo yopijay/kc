7cb317ad682dc78006c4836c3cdc34e04ccb715d
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-04-18 08:30:32 +0800

    Database update

--- database-backup/tbl_category.sql
+++ database-backup/tbl_category.sql
@@ -12,7 +12,7 @@
  Target Server Version : 140003
  File Encoding         : 65001
 
- Date: 30/03/2023 16:44:29
+ Date: 18/04/2023 08:11:44
 */
 
 
@@ -26,7 +26,7 @@ MAXVALUE 2147483647
 START 1
 CACHE 1;
 
-SELECT setval('"public"."tbl_category_id_seq"', 4, false);
+SELECT setval('"public"."tbl_category_id_seq"', 1, false);
 
 
 -- ----------------------------
@@ -50,14 +50,6 @@ CREATE TABLE "public"."tbl_category" (
   "date_imported" timestamptz(6)
 )
 ;
-
--- ----------------------------
--- Records of tbl_category
--- ----------------------------
-INSERT INTO "public"."tbl_category" VALUES (2, 'CTGY-0000002', 'supplies', 'BREAKROOM', 'BR', 1, 1, NULL, NULL, NULL, '2023-02-14 10:55:10+08', NULL, NULL, NULL);
-INSERT INTO "public"."tbl_category" VALUES (3, 'CTGY-0000003', 'assets', 'OFFICE FURNITURE', 'FURN', 1, 1, 1, NULL, NULL, '2023-02-14 10:55:31+08', '2023-02-14 11:21:28+08', NULL, NULL);
-INSERT INTO "public"."tbl_category" VALUES (1, 'CTGY-0000001', 'supplies', 'OFFICE ESSENTIALS', 'ESTL', 1, 1, 1, NULL, NULL, '2023-02-14 10:54:54+08', '2023-02-14 11:21:48+08', NULL, NULL);
-INSERT INTO "public"."tbl_category" VALUES (4, 'CTGY-0000004', 'assets', 'TECHNOLOGY', 'TECH', 1, 1, 1, NULL, NULL, '2023-02-14 10:55:41+08', '2023-03-13 15:56:32+08', NULL, NULL);
 
 -- ----------------------------
 -- Primary Key structure for table tbl_category
--- database-backup/tbl_company.sql
+++ database-backup/tbl_company.sql
@@ -12,7 +12,7 @@
  Target Server Version : 140003
  File Encoding         : 65001
 
- Date: 30/03/2023 16:44:36
+ Date: 18/04/2023 08:11:55
 */
 
 
@@ -26,7 +26,7 @@ MAXVALUE 2147483647
 START 1
 CACHE 1;
 
-SELECT setval('"public"."tbl_company_id_seq"', 5, false);
+SELECT setval('"public"."tbl_company_id_seq"', 1, false);
 
 
 -- ----------------------------
@@ -51,15 +51,6 @@ CREATE TABLE "public"."tbl_company" (
   "date_imported" timestamptz(6)
 )
 ;
-
--- ----------------------------
--- Records of tbl_company
--- ----------------------------
-INSERT INTO "public"."tbl_company" VALUES (2, 'CMP-0000002', 1, 'S-POWER CORPORATION', NULL, NULL, 1, 1, NULL, NULL, NULL, '2023-01-27 11:37:14+08', NULL, NULL, NULL);
-INSERT INTO "public"."tbl_company" VALUES (1, 'CMP-0000001', 2, 'KC INDUSTRIAL CORPORATION', '#82 CORDILLERA ST. CORNER QUEZON AVE., QUEZON CITY', NULL, 1, 1, 1, NULL, NULL, '2023-01-27 11:36:08+08', '2023-02-07 11:11:16+08', NULL, NULL);
-INSERT INTO "public"."tbl_company" VALUES (3, 'CMP-0000003', 1, 'SYSTEMS POWERMARK CORPORATION', NULL, NULL, 0, 1, 1, NULL, NULL, '2023-01-27 11:37:23+08', '2023-02-23 10:14:53+08', NULL, NULL);
-INSERT INTO "public"."tbl_company" VALUES (5, 'CMP-0000005', 1, 'KENNETH LAND AND HOLDINGS INC.', NULL, NULL, 0, 1, 1, NULL, NULL, '2023-01-27 11:38:18+08', '2023-03-14 09:26:34+08', NULL, NULL);
-INSERT INTO "public"."tbl_company" VALUES (4, 'CMP-0000004', 1, 'KC ELECTRICAL INNOVATION, INC.', NULL, NULL, 0, 1, 1, NULL, NULL, '2023-01-27 11:38:03+08', '2023-03-28 15:48:07+08', NULL, NULL);
 
 -- ----------------------------
 -- Primary Key structure for table tbl_company
