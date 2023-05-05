9304c6ea1f7ba8086b85703f509e7043281ebe3b
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-05-05 16:50:30 +0800

    Update index.js

--- warehouse/src/core/api/index.js
+++ warehouse/src/core/api/index.js
@@ -3,3 +3,6 @@ import { api } from "core/function/global";
 import Env from '../constants/Env.json';
 
 const env = 'live'; // Environment
+
+export const authentication = async (data) => { return await api({ url: `${Env[env].url}/login`, method: 'post', data: data }).then(res => res.data); }
+export const schedule = async (date) => { return await api({ url: `${Env[env].url}/schedule/${date}`, method: 'get' }).then(res => res.data); }
