96dbc84e35d7d4a14d2e858b34c042f96fe84fc7
Kuya Pijay <53463564+yopijay@users.noreply.github.com>
2023-05-05 10:59:30 +0800

    Warehouse

---
+++ warehouse/src/App.jsx
@@ -0,0 +1,25 @@
+// Libraries
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+// Core
+// import { ProfilePrvdr } from "core/context/Profile";
+// import { GlobalPrvdr } from "core/context/Global";
+
+// Layouts
+// import Account from 'pages/account';
+// import Main from 'pages/main';
+
+const App = () => {
+    const client = new QueryClient();
+    
+    return ( 
+        <Router>
+            <QueryClientProvider client= { client }>
+                {/* <GlobalPrvdr><Routes><Route path= "*" element= { localStorage.getItem('token') ? <ProfilePrvdr><Main /></ProfilePrvdr> : <Account /> } /></Routes></GlobalPrvdr> */}
+            </QueryClientProvider>
+        </Router> 
+    ); 
+}
+
+export default App;
